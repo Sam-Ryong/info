5fab8f9439f97be49a5e7ee3fccb07610b358921
sam_ryong <zackinthebox@khu.ac.kr>
2024-11-25 16:17:13 +0900

    WASM AI 디자인 변경

--- src/components/Ask.js
+++ src/components/Ask.js
@@ -6,9 +6,10 @@ const Ask = () => {
       src="https://sam-ryong.github.io/info/ask"
       title="My iframe"
       style={{
-        width: "100%", // 원하는 너비 설정
+        width: "150%", // 원하는 너비 설정
         height: "30%", // 고정 높이 설정
         border: "none", // 테두리 제거
+        maxWidth: "500px",
       }}
     />
   );
