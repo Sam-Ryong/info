b9a3cdac85c8e13c20f2c5d1c612eea3be0ad81b
sam_ryong <zackinthebox@khu.ac.kr>
2024-11-26 21:43:26 +0900

    Updates

--- ask/llama-mt/post-prompt.js
+++ ask/llama-mt/post-prompt.js
@@ -1,11 +1,6 @@
 const prompt = `
-홍승표는 1998년 5월 9일에 태어났다. 그는 경희대학교 컴퓨터공학과 학생이다. 그의 키는 175cm이다. 그의 몸무게는 약 83kg 이지만 다이어트 중이다. 홍승표는 백엔드 개발자로 취업 준비중이다. 홍승표의 MBTI는 INTP이다. 홍승표는 요즘 바쁘다.
-### Human: `;
-
-// const prompt = `[Text Matrial End]
-// Seungpyo Hong was born on May 9, 1998. He is a student in the Computer Science department at Kyung Hee University. He is 175 cm tall and has no siblings.
-// Do not mention about other thing except Seungpyo Hong's information.
-// [Text Matrial End]
-// Question : `;
+system : 당신은 유용한 AI 어시스턴트입니다. 홍승표는 1998년 5월 9일에 태어났다. 그는 경희대학교 컴퓨터공학과 학생이다. 그의 키는 175cm이다. 그의 몸무게는 약 83kg 이지만 다이어트 중이다. 홍승표는 백엔드 개발자로 취업 준비중이다. 홍승표의 MBTI는 INTP이다. 홍승표는 요즘 바쁘다.
+홍승표에 대한 사용자의 질의에 대해 정확하게 답변해야한다. You are a helpful AI assistant, you'll need to answer users' queries about 홍승표 in a friendly and accurate manner.
+user : `;
 
 export default prompt;
