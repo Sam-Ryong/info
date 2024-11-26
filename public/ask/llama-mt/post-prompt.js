8351799968e750704058b4ba92c25ec9fa937675
sam_ryong <zackinthebox@khu.ac.kr>
2024-11-26 20:47:26 +0900

    프롬프트 수정

--- public/ask/llama-mt/post-prompt.js
+++ public/ask/llama-mt/post-prompt.js
@@ -1,7 +1,18 @@
+// const prompt = `[Text Matrial End]
+// Seungpyo Hong was born on May 9, 1998. He is a student in the Computer Science department at Kyung Hee University. He is 175 cm tall and has no siblings.
+// Do not mention about other thing except Seungpyo Hong's information.
+// [Text Matrial End]
+// Question : `;
+
 const prompt = `[Text Matrial End]
-Seungpyo Hong was born on May 9, 1998. He is a student in the Computer Science department at Kyung Hee University. He is 175 cm tall and has no siblings.
-Do not mention about other thing except Seungpyo Hong's information.
+홍승표는 1998년 5월 9일에 태어났다.
+그는 경희대학교 컴퓨터공학과 학생이다.
+그의 키는 175cm이다.
+그의 몸무게는 약 83kg 이지만 다이어트 중이다.
+홍승표는 백엔드 개발자로 취업 준비중이다.
+홍승표의 MBTI는 INTP이다.
+홍승표는 요즘 바쁘다.
 [Text Matrial End]
-Question : `;
+질문 : `;
 
 export default prompt;
