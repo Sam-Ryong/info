dc751d6c9a3706725d7ca9918e12fcfa52f16469
sam_ryong <zackinthebox@khu.ac.kr>
2024-11-29 00:45:12 +0900

    visit count 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Header.css";
 import Social from "../Social";
 import Container from "../Container";
@@ -7,9 +8,33 @@ import P from "../P";
 import Ask from "../Ask/Ask";
 
 const Header = () => {
+  const [visitorCount, setVisitorCount] = useState({ today: 0, total: 0 });
+  useEffect(() => {
+    // Fetch visitor count
+    const fetchVisitorCount = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_HOST}/visit`,
+          { withCredentials: true }
+        );
+        setVisitorCount(response.data);
+      } catch (error) {
+        console.error("Failed to fetch visitor count:", error);
+      }
+    };
+
+    fetchVisitorCount();
+  }, []);
+
   return (
     <header className="header">
       <Container>
+        <div className="visitor-count">
+          <span>
+            Today: {visitorCount.today} <br></br>
+            Total: {visitorCount.total}
+          </span>
+        </div>
         <a href="/" style={{ textDecoration: "none", color: "inherit" }}>
           <H1>홍삼룡의 블로그</H1>
         </a>
