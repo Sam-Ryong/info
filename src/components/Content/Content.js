b3b9cd58bc30bd4596524e9d9cfca468e3cdd0b3
sam_ryong <zackinthebox@khu.ac.kr>
2024-12-26 22:15:52 +0900

    직접 가져오기 방법

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -4,26 +4,28 @@ import "react-quill/dist/quill.snow.css";
 import "./Content.css";
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
-import { useSelector } from "react-redux";
 
 import { NotionRenderer } from "react-notion";
 
 const Content = () => {
   const { pageId } = useParams(); // URL에서 pageId 가져오기
-  const posts = useSelector((state) => state.posts || []); // Redux에서 posts 가져오기
 
-  // posts에서 해당하는 객체 찾기
-  const currentPost = posts.find((post) => post.url === pageId);
+  const [response, setResponse] = useState({});
 
-  if (!currentPost) {
-    return <div className="error">Content not found.</div>; // pageId에 해당하는 데이터가 없을 때
-  }
+  useEffect(() => {
+    const NOTION_PAGE_ID = pageId;
+    fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
+      .then((res) => res.json())
+      .then((resJson) => {
+        setResponse(resJson);
+      });
+  }, [pageId]);
 
   return (
     <main className="content">
       <div className="notion-dark-mode">
         <NotionRenderer
-          blockMap={currentPost.content} // content 렌더링
+          blockMap={response} // content 렌더링
           fullPage={true}
           hideHeader={true}
           theme="dark"
